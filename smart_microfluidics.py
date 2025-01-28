9c0fad7affdc7022435da009bc67178e811ac3ac
Leonardo Lavagna <llavagna@proton.me>
2025-01-28 17:13:44 +0100

    Update smart_microfluidics.py

--- smart_microfluidics.py
+++ smart_microfluidics.py
@@ -341,9 +341,9 @@ elif section == "Visualization":
         if target_feature_1 == target_feature_2:
             st.write(":red[INPUT ERROR. The selected variables cannot be equal.]")
         elif target_feature_1 != target_feature_2:
-            numeric_data = data.select_dtypes(include=['float64', 'int64']).dropna()
-            numeric_data = numeric_data.drop(columns=["SIZE","PDI"])
-            correlation_matrix = numeric_data.corr()
+            numeric_data_ = data.select_dtypes(include=['float64', 'int64']).dropna()
+            numeric_data_ = numeric_data_.drop(columns=["SIZE","PDI"])
+            correlation_matrix = numeric_data_.corr()
             target_features = [target_feature_1, target_feature_2]
             joint_correlation = correlation_matrix[target_features].drop(index=target_features).mean(axis=1)
             joint_correlation_df = joint_correlation.reset_index()
