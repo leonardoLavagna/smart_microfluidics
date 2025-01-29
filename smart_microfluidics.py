7b0fc6fe574246d24276f1df8a6390510e370ea6
Leonardo Lavagna <llavagna@proton.me>
2025-01-29 18:08:55 +0100

    Update smart_microfluidics.py

--- smart_microfluidics.py
+++ smart_microfluidics.py
@@ -8,10 +8,31 @@ import scipy.cluster.hierarchy as sch
 import pickle
 from sklearn.ensemble import RandomForestRegressor
 
-# Title of the app
+
+################################################
+# AUXILIARY FUNCTIONS
+################################################  
+def categorize_size(size):
+    if size < 100:
+        return 'S'
+    elif size < 200:
+        return 'M'
+    else:
+        return 'L'
+
+def categorize_pdi(pdi):
+    if pdi < 0.1:
+        return 'HMD'
+    elif 0.1 < pdi < 0.25:
+        return 'MD'
+    else:
+        return 'PLD'
+
+
+################################################
+# COLOPHON
+################################################                    
 st.title("Smart Microfluidics: Modeling and Visualization")
-        
-# Sidebar for navigation
 st.sidebar.title("Choose an Option")
 section = st.sidebar.selectbox(
     "Section:",
@@ -21,12 +42,14 @@ section = st.sidebar.selectbox(
         "Visualization",
     ],
 )
-        
-# Upload dataset section
+
+
+################################################
+# 1. DATA
+################################################
 if section == "Upload Dataset":
     st.header("Upload Your Dataset")
     uploaded_file = st.file_uploader("Drag and Drop your CSV file here", type=["csv"])
-
     if uploaded_file is not None:
         try:
             df = pd.read_csv(uploaded_file)
@@ -36,9 +59,25 @@ if section == "Upload Dataset":
         except Exception as e:
             st.error(f"Error reading the file: {e}")
     else:
-        st.info("Please upload a CSV file.")
+        st.info("Reading default CSV file...")
+        file_path = "data/data.csv"
+        data = pd.read_csv(file_path, encoding="latin1")
+        data = data.drop(columns=['FR-O', 'FR-W'])
+        data.BUFFER = data.BUFFER.astype(str).str.strip()
+        data.BUFFER = data.BUFFER.replace({'PBS\xa0': 'PBS'})
+        data.CHIP = data.CHIP.replace({'Micromixer\xa0': 'Micromixer'})
+        for col in data.columns:
+            if data[col].dtype == 'object':
+                data[col] = data[col].astype(str)
+                for col in data.columns:
+                    if data[col].dtype == 'object':
+                        data[col] = data[col].astype(str)
+        st.dataframe(df)
 
-# Modeling section
+
+################################################
+# 2.MODELS
+################################################
 if section == "Modeling":
     st.sidebar.title("Model Selection")
     option = st.sidebar.selectbox(
@@ -50,7 +89,7 @@ if section == "Modeling":
         ],
     )
         
-    # 1. Random forest regressor
+    # 2.1 Random forest regressor
     if option == "Random forest regressor":
         st.header("Random forest regressor")
         st.write("Using multiple decision trees in parallel and bagging this model provide robust predictions for `SIZE` and `PDI`.")
@@ -86,7 +125,7 @@ if section == "Modeling":
             st.write(f"`SIZE`: {size:.2f}")
             st.write(f"`PDI`: {pdi:.2f}")
         
-    # 2. XGBoost
+    # 2.2 XGBoost
     elif option == "XGBoost":
         st.header("XGBoost")
         st.write("eXtreme Gradient Boosting predictions for `SIZE` and `PDI`.")
@@ -123,7 +162,7 @@ if section == "Modeling":
             st.write(f"`SIZE`: {size:.2f}")
             st.write(f"`PDI`: {pdi:.2f}")
     
-    # 3. Inverse model
+    # 2.3 Inverse model
     elif option == "Inverse problem":
         st.header("Inverse problem")
         st.write("Inverse problem solver: given target `SIZE` and `PDI` returns predictions for the other numerical features.")
@@ -142,9 +181,10 @@ if section == "Modeling":
             prediction_df = pd.DataFrame(predictions, columns=["ESM", "HSPC", "CHOL", "PEG", "TFR", "FRR"])
             st.write(prediction_df)
 
-
-
-# Visualization section
+        
+################################################
+# 3. VISUALIZATIONS
+################################################
 elif section == "Visualization":
     st.sidebar.title("Visualization Options")
     option = st.sidebar.selectbox(
@@ -156,8 +196,7 @@ elif section == "Visualization":
         ],
     )
 
-    # Visualizations
-    # 1. Correlation heatmap
+    # 3.1 Correlation heatmap
     if option == "Correlation heatmaps":
         st.header("Correlation heatmap")
         st.write("Displays the correlation between numerical features in the dataset.")
@@ -174,11 +213,10 @@ elif section == "Visualization":
                 for col in data.columns:
                     if data[col].dtype == 'object':
                         data[col] = data[col].astype(str)
-
         formed = st.text_input("Are you interested in the dataset of formed liposomes? Answer YES (Y) or NO (N):", "Y")
         n_ids = st.number_input("Enter the number of formulations you desire to visualize:", min_value=9, max_value=15, value=10)
         if formed == "YES" or formed == "Y":
-            # 1.1(Y) Correlations by features
+            # 3.1.1 Correlations by features
             st.subheader("Correlations by features")
             numeric_cols = data.select_dtypes(include=['number']).columns
             yes_data = data[data['OUTPUT'] == 'YES']
@@ -187,7 +225,7 @@ elif section == "Visualization":
             plt.figure(figsize=(10, 8))
             sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="coolwarm")
             st.pyplot(plt)
-            # 1.2(Y) Correlations by IDs
+            # 3.1.2 Correlations by IDs
             st.subheader("Correlations by IDs")
             data_red = data[:n_ids]
             yes_data_red = data_red[data_red['OUTPUT'] == 'YES']
@@ -196,28 +234,27 @@ elif section == "Visualization":
             plt.figure(figsize=(10, 8))
             sns.heatmap(correlation_matrix_t, annot=True, fmt=".2f", cmap="coolwarm")
             st.pyplot(plt)
-            # 1.3(Y) Clustered correlations by features
+            # 3.1.3 Clustered correlations by features
             st.subheader("Clustered correlations by features")
             linkage_matrix = sch.linkage(numeric_yes_data, method='ward')
             sns.clustermap(numeric_yes_data.corr(),method='ward',cmap='coolwarm',annot=True,figsize=(10, 8))
             st.pyplot(plt)
-            # 1.2(Y) Clustered correlations by IDs
+            # 3.1.4 Clustered correlations by IDs
             st.subheader("Clustered correlations by IDs")
             linkage_matrix_red = sch.linkage(numeric_yes_data_red.T, method='ward')
             sns.clustermap(numeric_yes_data_red.T.corr(),method='ward',cmap='coolwarm',annot=True,figsize=(10, 8))
             st.pyplot(plt)
         else:
-            # Select numeric columns for correlation calculation corresponding to no-formation
+            # 3.1.5 Correlations by IDs
             numeric_cols = data.select_dtypes(include=['number']).columns
             no_data = data[data['OUTPUT'] == 'NO']
             numeric_no_data = no_data[numeric_cols]
-            # Plot heatmap
             correlation_matrix = numeric_no_data.corr()
             plt.figure(figsize=(10, 8))
             sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="coolwarm")
-            st.pyplot(plt)        
-            # Plot heatmap by IDs
-            st.subheader("Correlations by IDs")
+            st.pyplot(plt)
+            # 3.1.6 Clustered correlations by IDs
+            st.subheader("Clustered correlations by IDs")
             data_red = data[:n_ids]
             no_data_red = data_red[data_red['OUTPUT'] == 'NO']
             numeric_no_data_red = no_data_red[numeric_cols]
@@ -225,7 +262,8 @@ elif section == "Visualization":
             plt.figure(figsize=(10, 8))
             sns.heatmap(correlation_matrix_t, annot=True, fmt=".2f", cmap="coolwarm")
             st.pyplot(plt)
-    
+                
+    # 3.2 Alluvial plot
     elif option == "Alluvial plot":
         st.header("Alluvial plot")
         st.write("Displays the flow of categorical data using an alluvial plot.")
@@ -242,23 +280,9 @@ elif section == "Visualization":
                 for col in data.columns:
                     if data[col].dtype == 'object':
                         data[col] = data[col].astype(str)
-        def categorize_size(size):
-            if size < 100:
-                return 'S'
-            elif size < 200:
-                return 'M'
-            else:
-                return 'L'
         data['SIZE'] = data['SIZE'].apply(categorize_size)
-        def categorize_pdi(pdi):
-            if pdi < 0.1:
-                return 'HMD'
-            elif 0.1 < pdi < 0.25:
-                return 'MD'
-            else:
-                return 'PLD'
         data['PDI'] = data['PDI'].apply(categorize_pdi)
-        # 2.1 Sankey diagram  of two variables
+        # 3.2.1 Sankey diagram  of two variables
         st.subheader("Sankey diagram of two variables")
         source = st.selectbox("Choose the first categorical variable of interest.", ("ML", "CHIP", "BUFFER", "OUTPUT", "SIZE", "PDI"))
         target = st.selectbox("Choose the second categorical variable of interest (different from the first).", ("CHIP", "ML", "BUFFER", "OUTPUT", "SIZE", "PDI"))
@@ -274,7 +298,7 @@ elif section == "Visualization":
           sankey_fig = go.Figure(data=[go.Sankey(node=dict(pad=15,thickness=20,line=dict(color="black", width=0.5),label=categories),
                                                  link=dict(source=sources,target=targets,value=values))])
           st.plotly_chart(sankey_fig)
-          # 2.2 Sankey diagram of the ML->CHIP->BUFFER->OUTPUT flow
+          # 3.2.2 Sankey diagram of the ML->CHIP->BUFFER->OUTPUT flow
           st.subheader("Sankey diagram of the ML->CHIP->BUFFER->OUTPUT flow")
           value_counts = data.groupby(['ML', 'CHIP', 'BUFFER', 'OUTPUT']).size().reset_index(name='value')
           all_categories = []
@@ -299,6 +323,7 @@ elif section == "Visualization":
                                         link=dict(source=sources,target=targets,value=values))])
           st.plotly_chart(fig)
     
+    # 3.3 Feature importance
     elif option == "Feature importance":
         st.header("Feature importance with a Random Forest Regressor")
         st.write("Displays the importance of each feature for predicting a set of input targets.")
@@ -314,7 +339,7 @@ elif section == "Visualization":
                 for col in data.columns:
                     if data[col].dtype == 'object':
                         data[col] = data[col].astype(str)
-        # 3.1 Single target feature importance
+        # 3.3.1 Single target feature importance
         st.subheader("Single target feature importance")
         target_feature = st.selectbox("Select a target feature.", ("TLP", "ESM", "HSPC", "CHOL", "PEG", "FRR", "SIZE", "PDI"))
         numeric_data = data.select_dtypes(include=['float64', 'int64']).dropna()
@@ -334,22 +359,7 @@ elif section == "Visualization":
         plt.figure(figsize=(10, 8))
         sns.barplot(data=feature_importances, x='Importance', y='Feature', hue='Feature', palette='viridis', dodge=False, legend=False)
         st.pyplot(plt)
-        # 3.2 Two targets feature importance
-        #st.subheader("Two targets feature importance")
-        #target_feature_1 = st.selectbox("Select the first target feature.", ("TLP", "ESM", "HSPC", "CHOL", "PEG", "FRR", "SIZE", "PDI"))
-        #target_feature_2 = st.selectbox("Select the second target feature (different from the first).", ("TLP", "ESM", "HSPC", "CHOL", "PEG", "FRR", "SIZE", "PDI"))
-        #if target_feature_1 == target_feature_2:
-        #    st.write(":red[INPUT ERROR. The selected variables cannot be equal.]")
-        #elif target_feature_1 != target_feature_2:
-        #    correlation_matrix = numeric_data.corr()
-        #    target_features = [target_feature_1, target_feature_2]
-        #    joint_correlation = correlation_matrix[target_features].drop(index=target_features).mean(axis=1)
-        #    joint_correlation_df = joint_correlation.reset_index()
-        #    joint_correlation_df.columns = ['Feature', 'Mean Correlation']
-        #    joint_correlation_df = joint_correlation_df.sort_values(by='Mean Correlation', ascending=False)
-        #    plt.figure(figsize=(10, 6))
-        #    sns.barplot(data=joint_correlation_df, x='Mean Correlation', y='Feature', hue='Feature', palette='viridis', dodge=False, legend=False)
-        #    st.pyplot(plt)
+        # 3.3.2 Two targets feature importance
         available_features = numeric_data.columns.tolist()
         target_feature_1 = st.selectbox("Select the first target feature.", available_features)
         target_feature_2 = st.selectbox("Select the second target feature (different from the first).", available_features)
