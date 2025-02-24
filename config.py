15a2da527778831c498a7aced5331071bee7b3b6
Leonardo Lavagna <llavagna@proton.me>
2025-02-24 16:19:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-extended = True
+# Paths
+random_forest_model = "models/random_forest_model.pkl"
+xgboost_model = "models/xgboost_model.pkl"
+inverse_xgboost_model = "models/inverse_xgboost_model.pkl"
