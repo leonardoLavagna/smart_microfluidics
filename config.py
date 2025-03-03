92a6167cc887fe6665989e240603ea6f1e28a395
Leonardo Lavagna <llavagna@proton.me>
2025-03-03 13:59:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Paths
-file_path = "data/extended_data.csv"
-random_forest_model = "models/random_forest_model_extended_1.pkl"
-xgboost_model = "models/xgboost_model_extended_1.pkl"
-inverse_xgboost_model = "models/inverse_xgboost_model_extended_1.pkl"
+file_path = "data/data.csv"
+random_forest_model = "models/random_forest_model.pkl"
+xgboost_model = "models/xgboost_model.pkl"
+inverse_xgboost_model = "models/inverse_xgboost_model.pkl"
+ensamble_model = "models/ensamble_model_for_size_and_pdi"
