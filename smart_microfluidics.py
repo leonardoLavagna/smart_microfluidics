eba2f1516081c61b9deee8bd5d67478d69c0501f
Leonardo Lavagna <llavagna@proton.me>
2025-03-05 14:46:11 +0100

    Update smart_microfluidics.py

--- smart_microfluidics.py
+++ smart_microfluidics.py
@@ -292,13 +292,13 @@ if section == "Data Modeling":
         st.write("Try the `ensemble-pdi` model with your data.")
         with open(pdi_model, "rb") as file:
             model = pickle.load(file)
-        tlp = st.number_input("TLP", value=5.0, min_value=0.0, max_value=100.0, step=0.1)
-        esm = st.number_input("ESM", value=0.0, min_value=0.0, max_value=100.0, step=0.1)
-        hspc = st.number_input("HSPC", value=3.75, min_value=0.0, max_value=100.0, step=0.1)
-        chol = st.number_input("CHOL", value=0.0, min_value=0.0, max_value=100.0, step=0.1)
-        peg = st.number_input("PEG", value=1.25, min_value=0.0, max_value=100.0, step=0.1)
-        tfr = st.number_input("TFR", value=1.0, min_value=0.0, max_value=100.0, step=0.1)
-        frr = st.number_input("FRR", value=3.0, min_value=0.0, max_value=100.0, step=0.1)
+        tlp = st.number_input("TLP", value=5.0, min_value=0.0, max_value=100.0, step=0.1, key="tlp")
+        esm = st.number_input("ESM", value=0.0, min_value=0.0, max_value=100.0, step=0.1, key="esm")
+        hspc = st.number_input("HSPC", value=3.75, min_value=0.0, max_value=100.0, step=0.1, key="hspc")
+        chol = st.number_input("CHOL", value=0.0, min_value=0.0, max_value=100.0, step=0.1, key="chol")
+        peg = st.number_input("PEG", value=1.25, min_value=0.0, max_value=100.0, step=0.1, key="peg")
+        tfr = st.number_input("TFR", value=1.0, min_value=0.0, max_value=100.0, step=0.1, key="tfr")
+        frr = st.number_input("FRR", value=3.0, min_value=0.0, max_value=100.0, step=0.1, key="frr")
         if st.button("Predict"):
             input_data = pd.DataFrame({
                 "TLP": [tlp],
