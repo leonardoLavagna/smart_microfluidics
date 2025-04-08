5f8a1af834812cc89e5f91d6046d7f9ce60d3de3
Leonardo Lavagna <llavagna@proton.me>
2025-04-08 15:45:32 +0200

    Update smart_microfluidics.py

--- smart_microfluidics.py
+++ smart_microfluidics.py
@@ -427,12 +427,10 @@ if section == "Data Modeling":
         tfr = st.number_input("TFR", value=1.0, min_value=0.0, max_value=100.0, step=0.1, key="tfr_in")
         frr = st.number_input("FRR", value=3.0, min_value=0.0, max_value=100.0, step=0.1, key="frr_in")
         buffer = st.selectbox("AQUEOUS", ["PBS", "MQ"], key="buf_in")
-        pdi = 0.33
-        size = 0.0
         size_disabled = True if pdi > 0 else False
         pdi_disabled = True if size > 0 else False
-        size = st.number_input("SIZE", value=size, min_value=0.0, max_value=5000.0, step=10.0, disabled=size_disabled, key="siz_in")
-        pdi = st.number_input("PDI", value=pdi, min_value=0.0, max_value=1.0, step=0.1, disabled=pdi_disabled, key="pd_in")
+        size = st.number_input("SIZE", value=0.0, min_value=0.0, max_value=5000.0, step=10.0, key="siz_in")
+        pdi = st.number_input("PDI", value=0.33, min_value=0.0, max_value=1.0, step=0.1, key="pd_in")
         if st.button("Predict"):
             if pdi > 0.0 and size==0.0:  
                 input_data = pd.DataFrame({
